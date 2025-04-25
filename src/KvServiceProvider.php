a707ca07587d652fd55c7ae57d1f20964426dc60
Puneet Sharma <puneet@dreamcast.com>
2025-04-25 14:26:45 +0530

    kv service update may be fix

--- src/KvServiceProvider.php
+++ src/KvServiceProvider.php
@@ -9,13 +9,9 @@ class KvServiceProvider extends ServiceProvider
     use PublishesMigrations;
     public function boot()
     {
-        $this->publishes(
-            [
-                __DIR__ . '/Migrations/2025_04_25_000001_create_enviroments_table.php' =>
-                $this->app->databasePath('database/migrations/' . date('Y_m_d_His') . '_create_environments_table.php')
-            ],
-            'migrations'
-        );
+        $this->publishes([
+            __DIR__ . '/Migrations' => database_path('migrations'),
+        ], 'migrations');
         //        $this->registerMigrations(__DIR__ . '/Migrations');
         $this->loadViewsFrom(__DIR__ . '/Views', 'kv-db');
     }
