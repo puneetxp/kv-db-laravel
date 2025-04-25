be7f370b37c9d3d342f2efc1e5069caf658ab9ed
Puneet Sharma <puneet@dreamcast.com>
2025-04-25 14:08:24 +0530

    update

--- src/KvServiceProvider.php
+++ src/KvServiceProvider.php
@@ -9,9 +9,19 @@ class KvServiceProvider extends ServiceProvider
     use PublishesMigrations;
     public function boot()
     {
-        $this->registerMigrations(__DIR__ . '/Migrations');
+
+
+        $this->publishes(
+            [
+                __DIR__ . '/Migrations/2025_04_25_000001_create_envrioments_table.php' =>
+                $this->app->databasePath('database/migrations' . now()->format('Y_m_d_His') . '_create_envrioments_tabl.php')
+            ],
+            'migrations'
+        );
+//        $this->registerMigrations(__DIR__ . '/Migrations');
         $this->loadViewsFrom(__DIR__ . '/Views', 'kv-db');
     }
+
 
     public function register()
     {
